ab89a90488ba94c9f2f2a8fc4fe98d03efe276a7
huziliu <724938718@qq.com>
2019-01-03 09:22:57 +0800

    Demo

---
+++ app/src/main/java/com/example/hp/message_interception/Welcome.java
@@ -0,0 +1,29 @@
+package com.example.hp.message_interception;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                 Intent intent=new Intent(Welcome.this,Home.class);
+                 startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task,3000);
+
+    }
+}
